4634f0746dacaca37b073c416f720a976cdd2168
saviour623 <106999975+saviour623@users.noreply.github.com>
2022-09-30 08:57:35 +0100

    Update _strdup.c

--- c-program/_strdup.c
+++ c-program/_strdup.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	char *s;
 
-    s = _strdup("ALX SE IS GOOD YEAH");
+    s = _strdup("Hi! Keep pushing! Keep learning");
     if (s == NULL)
     {
         printf("failed to allocate memory\n");
